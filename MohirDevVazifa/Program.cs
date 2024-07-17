cee4ffabfe9b49ce5252c22fbbe2fd1fbe3905f9
TheBude <husaenbobmuratov192@gmail.com>
2024-07-17 19:15:33 +0500

    Add project files.

---
+++ MohirDevVazifa/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write(" Toq sonlar yig'indisini hisoblavchi dastur: ");
+int toqSon = Convert.ToInt32(Console.ReadLine());
+
+int hisob = 0;
+int i = 1;
+while (i < toqSon)
+{
+    hisob += i;
+    i += 2;
+}
+
+Console.WriteLine($"{toqSon} gacha bo'lgan toqson yig'indisi: {hisob}");
